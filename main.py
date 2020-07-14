6bc2f4b019777107aa4378db4ec83043de3e910e
perry <pasibeybonsu@drew.edu>
2020-07-14 16:50:12 -0400

    Updated to receive extension in request body and create label

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
-from flask import request, send_from_directory
+from flask import request, send_from_directory, jsonify
 from flask_api import FlaskAPI
+from flask_cors import CORS
 
 app = FlaskAPI(__name__)
+CORS(app)
 
 directory = os.getcwd()
 
@@ -13,11 +15,16 @@ app.config["COMPLETE_STACK"] = os.path.join(directory, "complete")
 
 def to_md(filename):
     if request.method == 'POST':
+        # print(request.form)
         if request.files:
+            #Now receiving extension within request body
+            extension = request.form['extension']
+            #creating file label with extension data
+            label = '{filename}.{extension}'.format(filename=filename,extension=extension)
             file = request.files["file"]
-            file.save(os.path.join(app.config["STAGING_STACK"], filename))
+            file.save(os.path.join(app.config["STAGING_STACK"], label))
+        return {'message': 'File received'}
 
-            return {'message' : 'File received'}
 
 if __name__ == "__main__":
     app.run(debug=True)
